eeaf6fface18967e1747d9ebdb8e4c43fd2a13bc
Juli <jvshirobokova@gmail.com>
2021-01-30 00:24:46 +0100

    Adding getRandomNumber function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-let min;
-let max;
-function random(min, max) {
-	let rand = min + Math.random() * (max + 1 - min);
-	if (min<max && min>0){
-    return rand;
+function getRandomNumber(min, max) {
+	if (min<max && min>0) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
 
-	} else {
-	 
-     return "The min should be lower than max";
-	}
-	
+	  } else {
+
+ return "The min should be lower than max";
+	  }
+
 
 } //solution from https://learn.javascript.ru/number
+
+
+
+getRandomNumber(5,8);
