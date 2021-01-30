c1b53b82972bc1df0520172071fc026ad2052d7a
Juli <jvshirobokova@gmail.com>
2021-01-30 17:41:16 +0100

    Adding getRandomFloat function corrections

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-function getRandomNumber(min, max) {
-	if (min<max && min>0) {
-    return Math.random() * (max + 1 - min) + min;
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+//source https://learn.javascript.ru/task/random-int-min-max
+function getRandomInt(min, max) {
+  // получить случайное число от (min-0.5) до (max+0.5)
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  if (min<max && min>0) {
+    return Math.round(rand);
+  } else if (min<0) {
+ return "The min should greater be lower than zero";
+    } else {
+      return "The min should be lower than max";
+    }
 
-	  } else {
+}
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+//source https://myrusakov.ru/js-random-numbers.html
 
- return "The min should be lower than max";
-	  }
+function getRandomFloat(min, max) {
+  // получить случайное число от (min-0.5) до (max+0.5)
+  if (min<max && min>0) {
+    return Math.random() * (max - min) + min;
+  } else if (min<0) {
+ return "The min should greater be lower than zero";
+    } else {
+      return "The min should be lower than max";
+    }
+
+}
 
 
-} //solution from https://learn.javascript.ru/number
 
 
-
-getRandomNumber(5,8);
